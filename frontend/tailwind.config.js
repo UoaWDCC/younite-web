7daeac048c640d4bd8ae779e7c2503b5df3ce859
Matthew Tao <matthew.yingtao@gmail.com>
2023-05-19 10:52:56 +1200

    added gutter to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			spacing: {
+				gutter: "var(--gutter)",
+			},
+			gridTemplateColumns: {
+				14: "var(--gutter) repeat(12, minmax(0, 1fr)) var(--gutter)",
+			},
+		},
+	},
+	plugins: [],
+};
