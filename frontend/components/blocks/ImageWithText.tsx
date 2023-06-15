f876409b1fa4e7795085eca8148abc1a9710164f
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-06-16 00:31:57 +1200

    pushing, so i can pull updated content

--- frontend/components/blocks/ImageWithText.tsx
+++ frontend/components/blocks/ImageWithText.tsx
@@ -1,15 +1,20 @@
-export default function ImageWithText({ props }: { props: any }) {
-	const imageUrl = props.image.data.attributes.formats.large.url;
+import styles from "./ImageWithText.module.css";
 
-	return (
-		<div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
-			<div>{props.content}</div>
-			<div>
-				<img
-					style={{ display: "block", width: "100%" }}
-					src={`http://localhost:1337${imageUrl}`}
-				/>
-			</div>
-		</div>
-	);
+export default function ImageWithText({ props }: { props: any }) {
+  const imageUrl = props.image.data.attributes.formats.large.url;
+  console.log(props);
+  return (
+    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+      <div>
+        <img
+          className={styles.backgroundImg}
+          src={`http://localhost:1337${imageUrl}`}
+        />
+      </div>
+      <div>
+        <div className={styles.heading}>{props.Heading}</div>
+        <div className={styles.content}>{props.content}</div>
+      </div>
+    </div>
+  );
 }
