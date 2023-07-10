8858ec624d24ecf1ed741f72d7d28c693b9b34c3
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-07-11 06:01:36 +1200

    Fixed the layout sligtly, little more responsive and clear to user

--- frontend/components/feedback/FeedbackForm.tsx
+++ frontend/components/feedback/FeedbackForm.tsx
@@ -1,27 +1,31 @@
 import styles from './FeedbackForm.module.css';
 
 const FeedbackForm: React.FC = () => {
-    return (
-        <div className={styles.feedbackForm}>
-            <form>
-                <div>
-                    <label>Name:</label>
-                    <input type="text" required className={styles.inputField} />
-                </div>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" required className={styles.inputField} />
-                </div>
-                <div>
-                    <label>Feedback:</label>
-                    <textarea required className={styles.inputField} />
-                </div>
-                <div>
-                    <input type="submit" value="Submit" className={styles.submitButton} />
-                </div>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <div className={styles.feedbackForm}>
+        <h1 className={styles.header}>Submit Your Feedback!</h1>
+        <form>
+          <div>
+            <label className={styles.inputFieldLabel}>Name:</label>
+            <input type="text" required placeholder="Enter your name" className={styles.inputField} />
+          </div>
+          <div>
+            <label className={styles.inputFieldLabel}>Email:</label>
+            <input type="email" required placeholder="Enter your email" className={styles.inputField} />
+          </div>
+          <div>
+            <label className={styles.inputFieldLabel}>Message:</label>
+            <textarea required placeholder="Enter your message" className={styles.inputField}></textarea>
+          </div>
+          <div className={styles.submitButtonContainer}>
+            <input type="submit" value="Submit" className={styles.submitButton} />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackForm;
+
