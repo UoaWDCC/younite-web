d43d76193933136adede7212b705703ae11425be
Matthew Tao <matthew.yingtao@gmail.com>
2023-07-11 13:17:12 +1200

    added project main page skeleton

---
+++ frontend/app/projects/page.tsx
@@ -0,0 +1,41 @@
+import styles from "@/app/page.module.css";
+import Header from "@/components/header/header";
+import Link from "next/link";
+import { z } from "zod";
+import { projectSchema } from "./schemas";
+
+async function getData() {
+	const res = await fetch(
+		`http://localhost:1337/api/project-pages?populate[blocks][populate]=*`,
+		{
+			headers: {
+				authorization: "Bearer " + process.env.STRAPI_KEY,
+			},
+			cache: "no-cache",
+		}
+	);
+
+	const json = await res.json();
+	const data = json.data.map((p: any) => p.attributes);
+
+	const schema = z.array(projectSchema);
+
+	return schema.parse(data);
+}
+
+export default async function Home() {
+	const data = await getData();
+
+	return (
+		<main className={styles.main}>
+			<Header />
+			<div>
+				{data.map((p) => (
+					<div key={p.slug}>
+						<Link href={`/projects/${p.slug}`}>{p.title}</Link>
+					</div>
+				))}
+			</div>
+		</main>
+	);
+}
