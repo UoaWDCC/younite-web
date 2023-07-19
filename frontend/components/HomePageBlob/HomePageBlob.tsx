a228c40b5672abd17f3702b70613042f25d3417b
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2023-07-20 05:20:18 +1200

    SVG image correction, positioning of layout change  and attempted to optimise for mobile view

--- frontend/components/HomePageBlob/HomePageBlob.tsx
+++ frontend/components/HomePageBlob/HomePageBlob.tsx
@@ -1,25 +1,33 @@
-import styles from "./HomePageBlob.module.css";
+import styles from "./HomePageBlob.module.css"; // Import the CSS module
 
 const HomePageBlob: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={`${styles.blob} ${styles.blob1}`}>
+      <div className={`${styles.blob} ${styles.whiteBlob} ${styles.blob1}`}>
         <p>
           We are a group of young people who are passionate about creating
           positive change in our communities and beyond.
         </p>
       </div>
       <div className={styles.connector}>
-        <img src="/connector.svg" alt="Connector SVG" />
+        <img src="/img.svg" alt="Image connecting the two blobs" />
       </div>
-      <div className={`${styles.blob} ${styles.blob2}`}>
+      <div className={`${styles.blob} ${styles.whiteBlob} ${styles.blob2}`}>
         <p>
           Friends, Empowerment, Growth
           <br />
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore
         </p>
-        <button>JOIN US NOW</button>
+        <button className={styles.button}>JOIN US NOW</button> {/* Add the button class */}
+      </div>
+      <div className={`${styles.blob} ${styles.orangeBlob} ${styles.blob3}`}>
+        <p>
+          How do we make an impact?
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore
+        </p>
       </div>
     </div>
   );
