7e6533d1eef78fe21c733c1310747530b110e7a0
dylanheron <dylanheron0902@gmail.com>
2023-08-01 02:07:35 +1200

    Arrow SVG's swapped, and carousel horizontal margins added

--- frontend/components/CarouselBase.tsx
+++ frontend/components/CarouselBase.tsx
@@ -22,21 +22,23 @@ export default function CarouselBase({
 
 	return (
 		<div>		
-			<div className="text-blue-800 font-semibold text-4xl pl-20 pr-20 pt-28 bg-white bg-opacity-50 flex flex-row justify-between">
+			<div className="text-blue-800 font-semibold text-4xl pr-6 pt-28 flex flex-row justify-between">
 				<p>
 					Upcoming Events
 				</p>
 				<div>
-					<button className="pr-5"onClick={scrollPrev}>
-						<svg fill="#1E40AF" height="40" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-							<path d="M256 0C114.623 0 0 114.616 0 256s114.623 256 256 256c141.391 0 256-114.616 256-256S397.391 0 256 0zm122.268 287.116c0 8.666-7.028 15.68-15.68 15.68h-89.334l10.898 46.623c1 4.312-.71 8.798-4.332 11.348a10.842 10.842 0 0 1-12.131.24l-131.913-95.225a12.073 12.073 0 0 1 0-19.566l131.913-95.231a10.84 10.84 0 0 1 12.131.246 10.868 10.868 0 0 1 4.332 11.348l-10.898 46.623h89.334c8.652 0 15.68 7.014 15.68 15.68v62.234z"/>
+					<button className="-mr-0.5"onClick={scrollPrev}>
+						<svg width="77" height="41" viewBox="0 0 77 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M0.324324 15.5676C0.324324 7.14896 7.14896 0.324324 15.5676 0.324324H76.1707V40.5993H15.5676C7.14897 40.5993 0.324324 33.7746 0.324324 25.356V15.5676Z" stroke="#014788" stroke-width="0.648649"/>
+							<path d="M31.6088 25.311L26.4785 19.9856L31.6088 14.6602M27.1911 19.9856H47.3353" stroke="#014788" stroke-width="1.88514" stroke-linecap="round" stroke-linejoin="round"/>
 						</svg>
 					</button>	
 					<button onClick={scrollNext}>
-						<svg fill="#1E40AF" height="40" width="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-							<path  d="M16 0C7.164 0 0 7.164 0 16s7.164 16 16 16 16-7.164 16-16S24.836 0 16 0zm.031 24v-6H8v-4h8.031V8.031L24 16l-7.969 8z"/>
+						<svg width="78" height="41" viewBox="0 0 78 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M1.23253 0.324324H61.8356C70.2542 0.324324 77.0789 7.14896 77.0789 15.5676V25.356C77.0789 33.7746 70.2542 40.5993 61.8356 40.5993H1.23253V0.324324Z" stroke="#014788" stroke-width="0.648649"/>
+							<path d="M45.0494 14.1265L50.1797 19.4519L45.0494 24.7773M49.4671 19.4519L29.3229 19.4519" stroke="#014788" stroke-width="1.88514" stroke-linecap="round" stroke-linejoin="round"/>
 						</svg>
-					</button>				
+					</button>			
 				</div>
 			</div>
 			<div className={`overflow-hidden ${wrapperClass}`} ref={emblaRef}>
@@ -46,6 +48,9 @@ export default function CarouselBase({
 					{children}
 				</div>
 			</div>
+			<svg width="30" height="62" viewBox="0 0 30 62" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M30 31L-2.02656e-06 61.3109L-2.02656e-06 0.689111L30 31Z" fill="white"/>
+			</svg>
 	</div>
 
 	);
