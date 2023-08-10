b7280c5d966499906e2ec160c5032c1e64498211
bryanp4502 <127185872+bryanp4502@users.noreply.github.com>
2023-08-10 13:31:49 +1200

    feedback form values are submittable
    feedback form values are able to be console logged to be saved for later use

--- frontend/components/feedback/FeedbackForm.tsx
+++ frontend/components/feedback/FeedbackForm.tsx
@@ -1,29 +1,57 @@
-import styles from './FeedbackForm.module.css';
+import styles from "./FeedbackForm.module.css";
 
-const FeedbackForm: React.FC = () => {
+export default function FeedbackForm() {
+  async function handleSubmit(data: FormData) {
+    "use server";
+    // const form = new FormData(e.target)
+
+    const name = data.get("name")
+    const email = data.get("email")
+    const message = data.get("message")
+
+    console.log({
+      name, email, message
+    });
+    
+  }
+
   return (
-      <div className={styles.feedbackForm}>
-        <h1 className={styles.header}>Submit Your Feedback!</h1>
-        <form>
-          <div>
-            <label className={styles.inputFieldLabel}>Name:</label>
-            <input type="text" required placeholder="Enter your full name" className={styles.inputField} />
-          </div>
-          <div>
-            <label className={styles.inputFieldLabel}>Email:</label>
-            <input type="email" required placeholder="Enter your email" className={styles.inputField} />
-          </div>
-          <div>
-            <label className={styles.inputFieldLabel}>Message:</label>
-            <textarea required placeholder="Enter your feedback" className={styles.textareaField}></textarea>
-          </div>
-          <div className={styles.submitButtonContainer}>
-            <input type="submit" value="Submit" className={styles.submitButton} />
-          </div>
-        </form>
-      </div>
+    <div className={styles.feedbackForm}>
+      <h1 className={styles.header}>Submit Your Feedback!</h1>
+      <form action={handleSubmit}>
+        <div>
+          <label className={styles.inputFieldLabel}>Name:</label>
+          <input
+            name="name"
+            type="text"
+            required
+            placeholder="Enter your full name"
+            className={styles.inputField}
+          />
+        </div>
+        <div>
+          <label className={styles.inputFieldLabel}>Email:</label>
+          <input
+            name="email"
+            type="email"
+            required
+            placeholder="Enter your email"
+            className={styles.inputField}
+          />
+        </div>
+        <div>
+          <label className={styles.inputFieldLabel}>Message:</label>
+          <textarea
+            name="message"
+            required
+            placeholder="Enter your feedback"
+            className={styles.textareaField}
+          ></textarea>
+        </div>
+        <div className={styles.submitButtonContainer}>
+          <input type="submit" value="Submit" className={styles.submitButton} />
+        </div>
+      </form>
+    </div>
   );
-};
-
-export default FeedbackForm;
-
+}
