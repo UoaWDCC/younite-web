ec177f9209806ec128bb911dc317588dd98daea6
Matthew Tao <matthew.yingtao@gmail.com>
2023-08-10 13:50:57 +1200

    started members page

--- frontend/app/members/page.tsx
+++ frontend/app/members/page.tsx
@@ -1,69 +1,62 @@
+import RichText from "@/components/blocks/RichText";
+import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
-import { Inter } from "next/font/google";
 import Image from "next/image";
-import styles from "../page.module.css";
+import MembersBanner from "../../assets/membersbanner.jpg";
+import styles from "./page.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// async function getData() {
+// 	const res = await fetch(
+// 		`http://localhost:1337/api/home-page?populate[textWithImage][populate]=*`,
+// 		{
+// 			headers: {
+// 				authorization: "Bearer " + process.env.STRAPI_KEY,
+// 			},
+// 			cache: "no-cache",
+// 		}
+// 	);
+
+// 	const json = await res.json();
+// 	const attributes = json.data.attributes;
+
+// 	const schema = z.object({
+// 		heroParagraph: z.string(),
+// 		blob1: z.string(),
+// 		blob2: z.string(),
+// 		blob3: z.string(),
+// 		textWithImage: z.any(),
+// 	});
+
+// 	return schema.parse(attributes);
+// }
 
 export default async function Home() {
+	// const data = await getData();
+
 	return (
-		<main className={styles.main}>
+		<main className={`${styles.main} bg-gradient-1 isolate min-h-full`}>
 			<Header />
-			MEMBERS
-			<div className={styles.center}>
-				<Image
-					className={styles.logo}
-					src="/next.svg"
-					alt="Next.js Logo"
-					width={180}
-					height={37}
-					priority
-				/>
-				<div className={styles.thirteen}>
-					<Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+			<div className="flex flex-col items-center justify-center gap-6">
+				<h1 className="uppercase flex flex-col items-center mx-auto mt-12 mb-24">
+					<span className="text-6xl leading-[0.95]">Meet the</span>
+					<span className="text-8xl font-bold leading-[0.95]">2023 Team</span>
+				</h1>
+
+				<div className="relative w-full max-w-6xl grid place-items-center">
+					<Image className="w-full max-w-5xl" src={MembersBanner} alt="" />
+					<div className="absolute bottom-8 right-0 p-16 max-w-lg bg-white shadow-lg rounded-3xl text-b-dark-blue __markdown">
+						<RichText
+							props={{
+								text: "# Meet the 2023 YOUNITE Team!\n\nA group of young people eager to enact positive change in the Devonport-Takapuna community. Believing in youth voices and youth leadership.",
+							}}
+						/>
+						<button className="bg-b-blue px-5 py-3 rounded-full font-bold">
+							JOIN US NOW
+						</button>
+					</div>
 				</div>
 			</div>
-			<div className={styles.grid}>
-				<a
-					href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={inter.className}>
-						Docs <span>-&gt;</span>
-					</h2>
-					<p className={inter.className}>
-						Find in-depth information about Next.js features and API.
-					</p>
-				</a>
-
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={inter.className}>
-						Templates <span>-&gt;</span>
-					</h2>
-					<p className={inter.className}>Explore the Next.js 13 playground.</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2 className={inter.className}>
-						Deploy <span>-&gt;</span>
-					</h2>
-					<p className={inter.className}>
-						Instantly deploy your Next.js site to a shareable URL with Vercel.
-					</p>
-				</a>
-			</div>
+			<Footer />
 		</main>
 	);
 }
