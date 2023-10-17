59265ec1351648d08ad715d56b6a06e1d794af5a
Matthew Tao <matthew.yingtao@gmail.com>
2023-10-17 14:09:54 +1300

    ignore form type error

--- frontend/components/feedback/FeedbackForm.tsx
+++ frontend/components/feedback/FeedbackForm.tsx
@@ -1,57 +1,59 @@
 import styles from "./FeedbackForm.module.css";
 
 export default function FeedbackForm() {
-  async function handleSubmit(data: FormData) {
-    "use server";
-    // const form = new FormData(e.target)
+	async function handleSubmit(data: FormData) {
+		"use server";
+		// const form = new FormData(e.target)
 
-    const name = data.get("name")
-    const email = data.get("email")
-    const message = data.get("message")
+		const name = data.get("name");
+		const email = data.get("email");
+		const message = data.get("message");
 
-    console.log({
-      name, email, message
-    });
-    
-  }
+		console.log({
+			name,
+			email,
+			message,
+		});
+	}
 
-  return (
-    <div className={styles.feedbackForm}>
-      <h1 className={styles.header}>Submit Your Feedback!</h1>
-      <form action={handleSubmit}>
-        <div>
-          <label className={styles.inputFieldLabel}>Name:</label>
-          <input
-            name="name"
-            type="text"
-            required
-            placeholder="Enter your full name"
-            className={styles.inputField}
-          />
-        </div>
-        <div>
-          <label className={styles.inputFieldLabel}>Email:</label>
-          <input
-            name="email"
-            type="email"
-            required
-            placeholder="Enter your email"
-            className={styles.inputField}
-          />
-        </div>
-        <div>
-          <label className={styles.inputFieldLabel}>Message:</label>
-          <textarea
-            name="message"
-            required
-            placeholder="Enter your feedback"
-            className={styles.textareaField}
-          ></textarea>
-        </div>
-        <div className={styles.submitButtonContainer}>
-          <input type="submit" value="Submit" className={styles.submitButton} />
-        </div>
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.feedbackForm}>
+			<h1 className={styles.header}>Submit Your Feedback!</h1>
+			{/* @ts-ignore */}
+			<form action={handleSubmit}>
+				<div>
+					<label className={styles.inputFieldLabel}>Name:</label>
+					<input
+						name="name"
+						type="text"
+						required
+						placeholder="Enter your full name"
+						className={styles.inputField}
+					/>
+				</div>
+				<div>
+					<label className={styles.inputFieldLabel}>Email:</label>
+					<input
+						name="email"
+						type="email"
+						required
+						placeholder="Enter your email"
+						className={styles.inputField}
+					/>
+				</div>
+				<div>
+					<label className={styles.inputFieldLabel}>Message:</label>
+					<textarea
+						name="message"
+						required
+						placeholder="Enter your feedback"
+						className={styles.textareaField}
+					></textarea>
+				</div>
+				<div className={styles.submitButtonContainer}>
+					<input type="submit" value="Submit" className={styles.submitButton} />
+				</div>
+			</form>
+		</div>
+	);
 }
