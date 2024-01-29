c881be06ce19cffea7a659968b04f318b6625fe1
clear <alexwillmcleod@gmail.com>
2024-01-30 11:51:08 +1300

    deployed strapi to app engine for better cloud bucket support

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,15 +1,4 @@
 module.exports = ({ env }) => {
-  const serviceAccountJsonBase64 = env("GOOGLE_SERVICE_ACCOUNT");
-  const serviceAccountText = Buffer.from(
-    serviceAccountJsonBase64,
-    "base64"
-  ).toString();
-  console.log(serviceAccountText);
-
-  const serviceAccount = serviceAccountText && JSON.parse(serviceAccountText);
-
-  console.log(serviceAccount);
-
   return {
     upload: {
       config: {
@@ -19,9 +8,6 @@ module.exports = ({ env }) => {
           bucketName: "wdcc-younite-strapi-image-bucket",
           publicFiles: true,
           uniform: true,
-          serviceAccount: serviceAccount,
-          baseUrl:
-            "https://storage.googleapis.com/wdcc-younite-strapi-image-bucket",
           basePath: "",
         },
       },
