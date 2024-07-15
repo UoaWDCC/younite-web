2143bc52390cf296be6fe41e4528361ed9d2eb48
Oculux <nate.williamson.nz@gmail.com>
2024-07-16 11:50:50 +1200

    :art: simplify getLargestImageUrl function

--- next/util/image.ts
+++ next/util/image.ts
@@ -1,16 +1,7 @@
 import { ImageType } from "@/schemas/Image";
 
 export const getLargestImageUrl = (img: ImageType) => {
-  try {
-    const formats = img.data.attributes.formats;
-
-    const largestFormat = formats.large ??
-      formats.medium ??
-      formats.small ??
-      formats.thumbnail ?? { url: "" };
-
-    return `${process.env.STRAPI_URL}${largestFormat.url}`;
-  } catch {
-    return process.env.STRAPI_URL;
-  }
+  const { large, medium, small, thumbnail } = img.data.attributes.formats;
+  const largestFormat = large ?? medium ?? small ?? thumbnail ?? { url: "" };
+  return `${process.env.STRAPI_URL}${largestFormat.url}`;
 };
