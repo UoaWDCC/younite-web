1ebdb76e832689b69f9974078b52573e4a5d92a1
Oculux <nate.williamson.nz@gmail.com>
2024-07-15 20:22:45 +1200

    :wrench: copy over strapi fetchApi function

---
+++ next/shared/strapi.ts
@@ -0,0 +1,50 @@
+interface Props {
+  endpoint: string;
+  query?: Record<string, string>;
+  wrappedByKey?: string;
+  wrappedByList?: boolean;
+}
+
+/**
+ * Fetches data from the Strapi API
+ * @param endpoint - The endpoint to fetch from
+ * @param query - The query parameters to add to the url
+ * @param wrappedByKey - The key to unwrap the response from
+ * @param wrappedByList - If the response is a list, unwrap it
+ * @returns
+ */
+export default async function fetchApi<T>({
+  endpoint,
+  query,
+  wrappedByKey,
+  wrappedByList,
+}: Props): Promise<T> {
+  if (endpoint.startsWith("/")) {
+    endpoint = endpoint.slice(1);
+  }
+
+  const url = new URL(`${process.env.STRAPI_URL}/api/${endpoint}`);
+  console.log(url.toString());
+  if (query) {
+    Object.entries(query).forEach(([key, value]) => {
+      url.searchParams.append(key, value);
+    });
+  }
+  const res = await fetch(url.toString(), {
+    headers: {
+      Authorization: `Bearer ${process.env.STRAPI_KEY}`,
+    },
+  });
+
+  let data = await res.json();
+
+  if (wrappedByKey) {
+    data = data[wrappedByKey];
+  }
+
+  if (wrappedByList) {
+    data = data[0];
+  }
+
+  return data as T;
+}
