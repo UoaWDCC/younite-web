57dbfe41643fde21d90e3cada1c53caf44cd0ed2
Oculux <nate.williamson.nz@gmail.com>
2024-07-17 23:06:02 +1200

    :sparkles: make Nate's Component

--- next/app/test/nate/mycomponent/page.tsx
+++
@@ -1,5 +0,0 @@
-import NateComponent from "@/components/dev/NateComponent";
-
-export default function MyComponent() {
-  return <NateComponent />;
-}
--- next/components/dev/NateComponent.tsx
+++ next/components/dev/NateComponent.tsx
@@ -1,7 +1,3 @@
 export default function NateComponent() {
-  return (
-    <div>
-      <h1>Nate's Component</h1>
-    </div>
-  );
+  return <div>Nate</div>;
 }
