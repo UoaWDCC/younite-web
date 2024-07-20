ee688fe40d83afb013bacdfff9ac92c32e56f822
Deiza Talreja <deizat1111@gmail.com>
2024-07-20 22:00:24 +1200

    :sparkles: make Deiza's Component

--- next/app/test/nate/mycomponent/page.tsx
+++ next/app/test/nate/mycomponent/page.tsx
@@ -1,5 +1,13 @@
-import NateComponent from "@/components/dev/NateComponent";
+// import NateComponent from "@/components/dev/NateComponent";
+
+// export default function MyComponent() {
+//   return <NateComponent />;
+// }
+
+
+import DeizaComponent from "@/components/dev/DeizaComponent";
 
 export default function MyComponent() {
-  return <NateComponent />;
+  return <DeizaComponent />;
 }
+
---
+++ next/components/dev/DeizaComponent.tsx
@@ -0,0 +1,11 @@
+export default function DeizaComponent() {
+    return (
+      <div>
+        <h1>Deiza's Component</h1>
+        <p>Hello and Welcome :D This is my component!!</p>
+      </div>
+    );
+}
+
+  
+
