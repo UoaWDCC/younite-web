ce707210fa518e3e324b7a35dec74c7205d03aa8
Hayley Sharpe <sharpeh42@gmail.com>
2024-07-24 21:52:41 +1200

    :memo: create handleClick function, swap active page based on state

--- next/components/feedback/ActiveSection.tsx
+++ next/components/feedback/ActiveSection.tsx
@@ -16,18 +16,26 @@ export default function ActiveSection({
   const QAs = data.QuestionAnswer;
   const [activeSection, setActiveSection] = useState<"FAQ" | "Contact">("FAQ");
 
+  function handleClick() {
+    // added this fn
+    if (activeSection == "Contact") setActiveSection("FAQ");
+    else {
+      setActiveSection("Contact");
+    }
+  }
+
   return (
     <div className="flex py-24 w-full max-w-5xl mx-auto gap-12">
       <div className="flex flex-col items-end">
         <button
           className="font-bold text-7xl mb-2"
-          onClick={() => setActiveSection("FAQ")}
+          onClick={() => setActiveSection(activeSection)} // changed this to activeSection
         >
           FAQ
         </button>
         <button
           className="uppercase text-4xl font-bold"
-          onClick={() => setActiveSection("Contact")}
+          onClick={() => handleClick()} // changed this to handleClick
         >
           Contact
         </button>
