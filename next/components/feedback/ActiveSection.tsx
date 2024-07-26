6110e98f3433e6829cb1db75f144da6dcecbd4af
Hayley Sharpe <sharpeh42@gmail.com>
2024-07-26 20:01:44 +1200

    :lipstick: implement FAQ/Contact labels swapping over

--- next/components/feedback/ActiveSection.tsx
+++ next/components/feedback/ActiveSection.tsx
@@ -15,12 +15,19 @@ export default function ActiveSection({
 }) {
   const QAs = data.QuestionAnswer;
   const [activeSection, setActiveSection] = useState<"FAQ" | "Contact">("FAQ");
+  const [top_label, setTopLabel] = useState<"FAQ" | "Contact">("FAQ");
+  const [bottom_label, setBottomLabel] = useState<"FAQ" | "Contact">("Contact");
 
   function handleClick() {
     // added this fn
-    if (activeSection == "Contact") setActiveSection("FAQ");
-    else {
+    if (activeSection == "Contact") {
+      setActiveSection("FAQ");
+      setTopLabel("FAQ");
+      setBottomLabel("Contact");
+    } else {
       setActiveSection("Contact");
+      setTopLabel("Contact");
+      setBottomLabel("FAQ");
     }
   }
 
@@ -28,16 +35,16 @@ export default function ActiveSection({
     <div className="flex py-24 w-full max-w-5xl mx-auto gap-12">
       <div className="flex flex-col items-end">
         <button
-          className="font-bold text-7xl mb-2"
+          className="uppercase font-bold text-7xl mb-2"
           onClick={() => setActiveSection(activeSection)} // changed this to activeSection
         >
-          FAQ
+          {top_label}
         </button>
         <button
           className="uppercase text-4xl font-bold"
           onClick={() => handleClick()} // changed this to handleClick
         >
-          Contact
+          {bottom_label}
         </button>
       </div>
       <div className="flex-1 mr-40">
