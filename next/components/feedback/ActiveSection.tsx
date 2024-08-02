5c324be0ec296504e374473e8b3bcda813c0f9f6
zozzzC <czlc05edu@gmail.com>
2024-08-02 20:56:22 +1200

    :zap: change style state to boolean

--- next/components/feedback/ActiveSection.tsx
+++ next/components/feedback/ActiveSection.tsx
@@ -16,11 +16,11 @@ export default function ActiveSection({
   };
 }) {
   const QAs = data.QuestionAnswer;
-  const [style, setStyle] = useState<string>();
+  const [styleClick, setStyleClick] = useState<boolean>();
   const [activeSection, setActiveSection] = useState<"FAQ" | "Contact">("FAQ");
 
   function handleClick() {
-    setStyle("opacity-0 animate-[fadeOut_0.3s_ease-in]");
+    setStyleClick(true);
 
     setTimeout(() => {
       if (activeSection == "Contact") {
@@ -31,7 +31,7 @@ export default function ActiveSection({
     }, 300);
 
     setTimeout(() => {
-      setStyle("animate-[fadeIn_0.3s_ease-in]");
+      setStyleClick(false);
     }, 300);
   }
 
@@ -39,7 +39,7 @@ export default function ActiveSection({
     <div className="flex py-24 w-full max-w-5xl mx-auto gap-12">
       <div className="flex flex-col items-end">
         <button
-          className={"ml-5 uppercase font-bold text-7xl mb-2 " + style}
+          className={`ml-5 uppercase font-bold text-7xl mb-2 ${styleClick ? "animate-[fadeOut_0.3s_ease-in]" : "animate-[fadeIn_0.3s_ease-in]"}`}
           disabled={true}
         >
           {activeSection == "FAQ" ? "FAQ" : "Contact"}
@@ -48,12 +48,10 @@ export default function ActiveSection({
           <Image
             src={selectionArrow}
             alt="Selection arrow"
-            className={"inline-block " + style}
+            className={`inline-block ${styleClick ? "animate-[fadeOut_0.3s_ease-in]" : "animate-[fadeIn_0.3s_ease-in]"}`}
           />
           <button
-            className={
-              "text-b-dark-blue ml-5 uppercase text-4xl font-bold " + style
-            }
+            className={`text-b-dark-blue ml-5 uppercase text-4xl font-bold ${styleClick ? "animate-[fadeOut_0.3s_ease-in]" : "animate-[fadeIn_0.3s_ease-in]"}`}
             onClick={() => handleClick()}
           >
             {activeSection == "FAQ" ? "Contact" : "FAQ"}
