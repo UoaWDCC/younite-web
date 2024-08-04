421f5b34e636640e1f3910c8b24d265bdc9a91f7
Oculux <nate.williamson.nz@gmail.com>
2024-08-04 12:09:18 +1200

    :bug: revert header changes to avoid Next crashing

--- next/components/header/header.tsx
+++ next/components/header/header.tsx
@@ -1,9 +1,8 @@
-"use client"
 import { headerSchema } from "@/schemas/single/Header";
+import { getLargestImageUrl } from "@/util/image";
 import fetchStrapi from "@/util/strapi";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { z } from "zod";
 import styles from "./header.module.css";
 
@@ -20,33 +19,10 @@ async function getHeaderData() {
   };
 }
 
-export interface HeaderData {
-  members: any;
-  navigation: Navigation["link"];
-  Logo?: any;
-}
-
-export interface Navigation {
-  link: {
-    slug: string;
-    title: string;
-  }[];
-}
-
-export default function Header() {
-  const [data, setData] = useState<HeaderData>();
-  const [logoSrc, setLogoSrc] = useState<any>();
-  const [links, setLinks] = useState<Navigation["link"]>();
-
-  useEffect(() => {
-    async function fetchData() {
-      const result = await getHeaderData();
-      setData(result);
-      setLogoSrc(result.Logo)
-      setLinks(result.navigation)
-    }
-    fetchData();
-  }, []);
+export default async function Header() {
+  const data = await getHeaderData();
+  const logoSrc = getLargestImageUrl(data.Logo);
+  const links = data.navigation;
 
   return (
     <header className={styles.header}>
@@ -63,7 +39,7 @@ export default function Header() {
         <div className="group relative">
           <Link
             href={
-              data?.members[0]
+              data.members[0]
                 ? `/members/${data.members[0].CommitteeYear}`
                 : "/"
             }
@@ -80,7 +56,7 @@ export default function Header() {
           </div>
         </div>
 
-        {links?.map((link) => (
+        {links.map((link) => (
           <Link href={link.slug} key={link.title}>
             {link.title.toLocaleUpperCase()}
           </Link>
