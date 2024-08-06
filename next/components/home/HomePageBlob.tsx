1a170740d626a54c4297cb31685a0b4507503f15
belleyong <136943913+belleyong@users.noreply.github.com>
2024-08-06 22:08:02 +1200

    :art: refactor homepage blob to tailwind css

--- next/components/home/HomePageBlob.tsx
+++ next/components/home/HomePageBlob.tsx
@@ -14,13 +14,13 @@ export default function HomePageBlobs({ blob1, blob2, blob3 }: Props) {
     <div
       className={`${styles.container} grid grid-cols-14 my-24 p-5 drop-shadow-xl`}
     >
-      <Blob className={`bg-white ${styles.blob1}`}>{blob1}</Blob>
-      <ConnectorBlob className={styles.connector} />
-      <Blob className={`bg-white ${styles.blob2}`}>
+      <Blob className="bg-white col-start-8 col-span-4">{blob1}</Blob>
+      <ConnectorBlob className="col-start-10 col-end-12 justify-self-center" />
+      <Blob className="bg-white col-start-10 col-span-4">
         <RichText props={{ text: blob2 }} />
         <button className={styles.button}>JOIN US NOW</button>
       </Blob>
-      <Blob className={`bg-[#ffaa00] ${styles.blob3}`}>{blob3}</Blob>
+      <Blob className="bg-[#ffaa00] text-black row-start-4 col-start-2 col-span-4">{blob3}</Blob>
     </div>
   );
 }
