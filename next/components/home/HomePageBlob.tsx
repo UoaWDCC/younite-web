85730609b328fe7aa118a046a8c294a164ebd68f
Oculux <nate.williamson.nz@gmail.com>
2024-08-07 13:14:05 +1200

    :lipstick: make the connector hidden on mobile (temporary fix)

--- next/components/home/HomePageBlob.tsx
+++ next/components/home/HomePageBlob.tsx
@@ -15,7 +15,7 @@ export default function HomePageBlobs({ blob1, blob2, blob3 }: Props) {
       className={`${styles.container} grid grid-cols-14 my-24 p-5 drop-shadow-xl`}
     >
       <Blob className="bg-white col-start-8 col-span-4">{blob1}</Blob>
-      <ConnectorBlob className="col-start-10 col-end-12 justify-self-center" />
+      <ConnectorBlob className="col-start-10 col-end-12 justify-self-center hidden md:block" />
       <Blob className="bg-white col-start-10 col-span-4">
         <RichText props={{ text: blob2 }} />
         <button className={styles.button}>JOIN US NOW</button>
