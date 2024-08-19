bb8aa697e29bb1bea7696360ca19aeefd0ae0835
zozzzC <czlc05edu@gmail.com>
2024-08-19 20:08:46 +1200

    :sparkles: make richtext component render

--- next/components/home/Blob.tsx
+++ next/components/home/Blob.tsx
@@ -9,13 +9,12 @@ type BlobProps = {
 export default function Blob({ children, className = "" }: BlobProps) {
   // If children is a string, render it as a RichText component
   const richChildren = <RichText props={{ text: children }} />;
-  children = children instanceof String ? richChildren : children;
 
   return (
     <div
       className={`block rounded-3xl text-blue-950 text-lg p-8 [&_:is(h1, h2, h3)]:text-xl [&_:is(h1, h2, h3)]:font-bold ${className}`}
     >
-      {children}
+      {typeof children === "string" ? richChildren : children}
     </div>
   );
 }
