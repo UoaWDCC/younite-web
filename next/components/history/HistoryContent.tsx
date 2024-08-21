6c568a6e10b33ff3c8ff364f043f5dc8a50be4c6
unknown <annlowe789@gmail.com>
2024-08-21 14:21:07 +1200

    :lipstick: fix styling

--- next/components/history/HistoryContent.tsx
+++ next/components/history/HistoryContent.tsx
@@ -3,12 +3,13 @@ type HistoryContentProps = {
   description: string;
 }
 
+
+
 export default function HistoryContent({ title, description }: HistoryContentProps) {
 
   return (
     <div>
-      <div id="content" className="bg-[#FAAB36] p-5 rounded-3xl shadow-lg h-[170px] w-auto max-w-[350px] overflow-hidden py-1">
-        <h1 className="text-lg font-semibold mb-2 pt-2.5">{title}</h1>
+      <div className="bg-[#FAAB36] p-5 rounded-3xl shadow-lg h-[170px] w-auto max-w-[500px] overflow-hidden py-1">        <h1 className="text-lg font-semibold mb-2 pt-2.5">{title}</h1>
         <p className="mb-4 text-base font-normal ">
           {description}
         </p>
