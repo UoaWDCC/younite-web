e3e5fc6602ba9fabf6be636ba4aafec1acadb7f3
daisy-of-the-valley <deizat1111@gmail.com>
2024-08-27 15:57:30 +1200

    Merge branch '74_history-component' of https://github.com/UoaWDCC/younite-web into 74_history-component

--- next/app/test/deiza/historytest/page.tsx
+++ next/app/test/deiza/historytest/page.tsx
@@ -1,13 +1,27 @@
 import HistoryComponent from "@/components/history/HistoryComponent";
-import HistoryImage from "@/components/history/HistoryImage";
+import { TimelineElement } from "@/schemas/single/AboutPage";
 
 
-export default function HistoryTest() {
+export default async function HistoryTest() {
   // return HistoryImage({src: "/uploads/istockphoto_1413873774_612x612_27a467169b.jpg"});
   // return <HistoryImage src="/uploads/istockphoto_1413873774_612x612_27a467169b.jpg" />
   // return <HistoryImage src={process.env.STRAPI_URL + "/uploads/istockphoto_1413873774_612x612_27a467169b.jpg"} />
+
+  // const data = await fetchStrapi("about-page", aboutPageSchema);
+
+  // const timeline: TimelineElement[] = data.Timeline.map((e) => {
+  //   const date = new Date(e.Date);
+  //   return {
+  //     ...e,
+  //     Date: date,
+  //   };
+  // }).sort((a, b) => a.Date.getTime() - b.Date.getTime());
+
+  const timeline: TimelineElement[] =
+
+
   return (
-    <HistoryComponent />
+    <HistoryComponent timelineElements={timeline}/>
   )
 
 }
