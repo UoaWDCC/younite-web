08c60652426236b8eb99f157b079961b8c893754
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-08-27 20:31:26 +1200

    Setting up the testing interface under test/jess/mycomponent

--- next/components/dev/JessComponent.tsx
+++ next/components/dev/JessComponent.tsx
@@ -1,7 +1,12 @@
+import ProjectModal from '../projects/ProjectModal';
+
 export default function JessComponent() {
     return (
       <div>
-        <h1>Jess!!!</h1>
+        <ProjectModal
+        title="Project Title"
+        description="Project Content"
+        imageUrl="http://127.0.0.1:1337/uploads/priscilla_du_preez_n_F8xh_L_Mmg0c_unsplash_1_7b7bcfcb87.png"/>
       </div>
     );
   }
--- next/components/projects/ProjectModal.tsx
+++ next/components/projects/ProjectModal.tsx
@@ -1,5 +1,27 @@
 import Image from 'next/image';
 import React from 'react';
-import { useState } from "react";
 
+interface ModalProps {
+  // onClose: () => void;
+  title: string;
+  description: string;
+  imageUrl: string;
+  // signUpUrl: string;
+}
 
+const ProjectModal: React.FC<ModalProps> = ({ title, description, imageUrl }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <Image
+            src={imageUrl}
+            alt="Project Image"
+             width={100}
+             height={100}
+            />
+    </div>
+  );
+};
+
+export default ProjectModal;
