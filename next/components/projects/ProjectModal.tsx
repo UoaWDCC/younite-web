67b46cebae808a39cc1c4348a6f11a704a761611
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-08-27 20:50:46 +1200

    Got image showing up, but not text

--- next/components/projects/ProjectModal.tsx
+++ next/components/projects/ProjectModal.tsx
@@ -11,15 +11,45 @@ interface ModalProps {
 
 const ProjectModal: React.FC<ModalProps> = ({ title, description, imageUrl }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <Image
+    // <div>
+    //   <h2>{title}</h2>
+    //   <p>{description}</p>
+    //   <Image
+    //         src={imageUrl}
+    //         alt="Project Image"
+    //          width={100}
+    //          height={100}
+    //         />
+    // </div>
+
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div className="bg-white bg-opacity-67 rounded-lg overflow-hidden w-96 relative shadow-lg">
+          <div className="relative w-full h-48">
+          <Image
             src={imageUrl}
             alt="Project Image"
-             width={100}
-             height={100}
-            />
+            layout="fill"
+            objectFit="cover"
+          />
+
+        <div className="p-6 bg-gradient-to-t from-orange-200 to-blue-200">
+          <h2 className="text-white text-xl font-bold mb-4 text-center"> PROJECT TITLE </h2>
+          <p className="text-white text-center mb-6">
+            A group of young people eager to enact positive change in the
+            Devonport-Takapuna community. Believing in youth voices and youth
+            leadership.
+          </p>
+
+          <a
+            href="/signup" // Replace with your sign-up URL
+            className="block bg-blue-500 text-white text-center py-2 rounded-lg"
+          >
+            Click Here to Sign up
+          </a>
+        </div>
+
+        </div>
+      </div>
     </div>
   );
 };
