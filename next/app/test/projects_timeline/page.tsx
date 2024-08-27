2d803399e381aa38a2151877f7b0812c3a58fa1b
h4yleysh4rpe <sharpeh42@gmail.com>
2024-08-27 22:01:43 +1200

    :bricks: create timeline component, test page, add basic line element

---
+++ next/app/test/projects_timeline/page.tsx
@@ -0,0 +1,7 @@
+import Timeline from "@/components/projects/Timeline";
+import styles from "./styles.module.css";
+
+
+export default async function TestPage() {
+  return <Timeline />;
+}
---
+++ next/components/projects/Timeline.tsx
@@ -0,0 +1,10 @@
+const Timeline = ({}) => {
+  return (
+    <div className="flex flex-col outline relative mt-header flex-row w-1/5 items-center h-96 justify-center">
+      <div className="border-l-3 w-1 h-full bg-white"></div>
+      <div className="w-2/3"></div>
+    </div>
+  );
+};
+
+export default Timeline;
