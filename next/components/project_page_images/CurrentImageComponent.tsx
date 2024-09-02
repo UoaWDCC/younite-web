90436190abb628a562e1efbf4ccfc77f704d9512
unknown <annlowe789@gmail.com>
2024-09-03 01:13:08 +1200

    :memo: added comment note

--- next/components/project_page_images/CurrentImageComponent.tsx
+++ next/components/project_page_images/CurrentImageComponent.tsx
@@ -9,3 +9,8 @@ export default function CurrentImageComponent({
 }) {
   return <ImageComponent src={src} alt={alt} width={950} height={620} />;
 }
+
+// note:
+// having the same code for current and old image components (except for the w/h) doesn't seem
+// efficient, so maybe we can get rid of the current/old components and just have the basic
+// image component?
