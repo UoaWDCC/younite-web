aab6be33596cb7616b6380ccc55db155b20e7860
h4yleysh4rpe <sharpeh42@gmail.com>
2024-09-02 19:51:24 +1200

    :sparkles: create date blobs

--- next/app/test/projects_timeline/page.tsx
+++ next/app/test/projects_timeline/page.tsx
@@ -1,7 +1,12 @@
 import Timeline from "@/components/projects/Timeline";
+import DateBlob from "@/components/projects/DateBlob";
 import styles from "./styles.module.css";
 
-
 export default async function TestPage() {
-  return <Timeline />;
+  return (
+    <div className="flex-row-reverse items-center justify-center outline">
+      <Timeline />
+      <DateBlob />
+    </div>
+  );
 }
---
+++ next/components/projects/DateBlob.tsx
@@ -0,0 +1,11 @@
+const DateBlob = ({}) => {
+  return (
+    <div className="flex w-1/6 mt-header h-8">
+      <div className="flex items-center justify-center bg-white w-full h-full rounded-lg">
+        <div className="text-black text-sm">23 May, 2023</div>
+      </div>
+    </div>
+  );
+};
+
+export default DateBlob;
