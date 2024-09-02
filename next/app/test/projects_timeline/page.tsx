454d1c1e13454da8b04d4ddfaafce1d04145be3b
h4yleysh4rpe <sharpeh42@gmail.com>
2024-09-02 20:24:06 +1200

    :memo: get blobs to show up as many times as input data

--- next/app/test/projects_timeline/page.tsx
+++ next/app/test/projects_timeline/page.tsx
@@ -3,10 +3,18 @@ import DateBlob from "@/components/projects/DateBlob";
 import styles from "./styles.module.css";
 
 export default async function TestPage() {
+  var timelineData = [
+    { date: "May 23 2023" },
+    { date: "June 25 2023" },
+    { date: "July 18 2023" },
+  ];
+
   return (
     <div className="flex-row-reverse items-center justify-center outline">
       <Timeline />
-      <DateBlob />
+      {timelineData.map((data, idx) => (
+        <DateBlob data={data} key={idx} />
+      ))}
     </div>
   );
 }
