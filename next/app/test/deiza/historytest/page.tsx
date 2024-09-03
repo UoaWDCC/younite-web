eb68a2bfa24d668d7b408592543499d635f2fd06
Oculux <nate.williamson.nz@gmail.com>
2024-09-04 01:56:21 +1200

    :memo: comment HistoryComponent

--- next/app/test/deiza/historytest/page.tsx
+++ next/app/test/deiza/historytest/page.tsx
@@ -3,29 +3,16 @@ import { aboutPageSchema, TimelineElement } from "@/schemas/single/AboutPage";
 import fetchStrapi from "@/util/strapi";
 
 export default async function HistoryTest() {
-  // return HistoryImage({src: "/uploads/istockphoto_1413873774_612x612_27a467169b.jpg"});
-  // return <HistoryImage src="/uploads/istockphoto_1413873774_612x612_27a467169b.jpg" />
-  // return <HistoryImage src={process.env.STRAPI_URL + "/uploads/istockphoto_1413873774_612x612_27a467169b.jpg"} />
-
   const data = await fetchStrapi("about-page", aboutPageSchema);
 
-  const timeline: TimelineElement[] = data.Timeline.map((e) => {
+  // Get Timeline part of About Page (with Date objects)
+  const timelineElements: TimelineElement[] = data.Timeline.map((e) => {
     const date = new Date(e.Date);
     return {
       ...e,
       Date: date,
     };
-  }).sort((a, b) => a.Date.getTime() - b.Date.getTime());
+  });
 
-  // console.log(timeline);
-  // const timeline: TimelineElement[] = [];
-
-  // ({ Date: Date; Image: string; Date: Date; Title: string; Description: string; })
-
-  return (
-    <HistoryComponent timelineElements={timeline} />
-    // <div>
-    //   Hi
-    // </div>
-  );
+  return <HistoryComponent timelineElements={timelineElements} />;
 }
--- next/components/history/HistoryComponent.tsx
+++ next/components/history/HistoryComponent.tsx
@@ -13,32 +13,15 @@ type HistoryComponentProps = {
 export default function HistoryComponent({
   timelineElements,
 }: HistoryComponentProps) {
-  const fullTimeline: TimelineElement[] = timelineElements
-    .map((element) => {
-      const date = new Date(element.Date);
-      return {
-        ...element,
-        Date: date,
-      };
-    })
-    .sort((a, b) => a.Date.getTime() - b.Date.getTime());
+  // Sort timeline by date
+  const sortedTimelineElements: TimelineElement[] = timelineElements.sort(
+    (a, b) => a.Date.getTime() - b.Date.getTime(),
+  );
 
-  const topTimeline: TimelineElement[] = [];
-  const bottomTimeline: TimelineElement[] = [];
-
-  for (let i = 0; i < fullTimeline.length; i++) {
-    if (i % 2 == 0) {
-      topTimeline.push(fullTimeline[i]);
-    } else {
-      bottomTimeline.push(fullTimeline[i]);
-    }
-  }
-
-  // console.log("top timeline", topTimeline);
-  // console.log(bottomTimeline);
-
-  const topReactNodes = topTimeline.map((element) => {
+  // Map timeline elements to components
+  const timelineCcomponents = sortedTimelineElements.map((element) => {
     if (element.__component == "project-timeline.text-timeline-item") {
+      // Text components
       const textElement = element as TextTimelineElement;
       return (
         <HistoryText
@@ -48,32 +31,28 @@ export default function HistoryComponent({
         />
       );
     } else {
+      // Image components
       const imageElement = element as ImageTimelineElement;
       return <HistoryImage src={imageElement.Image} hasLineAbove={true} />;
     }
   });
 
-  const bottomReactNodes = bottomTimeline.map((element) => {
-    if (element.__component == "project-timeline.text-timeline-item") {
-      const textElement = element as TextTimelineElement;
-      return (
-        <HistoryText
-          title={textElement.Title}
-          description={textElement.Description}
-          hasLineAbove={false}
-        />
-      );
+  // Split components into top and bottom
+  const topTimeline: React.ReactNode[] = [];
+  const bottomTimeline: React.ReactNode[] = [];
+  for (let i = 0; i < timelineCcomponents.length; i++) {
+    if (i % 2 == 0) {
+      topTimeline.push(timelineCcomponents[i]);
     } else {
-      const imageElement = element as ImageTimelineElement;
-      return <HistoryImage src={imageElement.Image} hasLineAbove={false} />;
+      bottomTimeline.push(timelineCcomponents[i]);
     }
-  });
+  }
 
   return (
     <div>
-      <div className="flex">{topReactNodes}</div>
+      <div className="flex">{topTimeline}</div>
       <div className="bg-white h-[1px] w-1/2 mt-80 mx-32" />;
-      <div className="flex">{bottomReactNodes}</div>
+      <div className="flex">{bottomTimeline}</div>
     </div>
   );
 }
