06d14cb989a82dfac1180a40c0a27c346dee371a
zozzzC <czlc05edu@gmail.com>
2024-09-03 12:31:08 +1200

    :lipstick: add previous projects page description

--- next/components/projects/PreviousProjects.tsx
+++ next/components/projects/PreviousProjects.tsx
@@ -1,7 +1,13 @@
 export default function PreviousProjects({ projects }: { projects: any }) {
   return (
-    <div className="flex flex-col mt-header">
-      <p className="">Past Projects</p>
+    <div className="flex flex-col mt-header items-center justify-center text-center">
+      <p className="text-6xl font-bold leading-[0.95]">PREVIOUS PROJECTS</p>
+      <div className="max-w-4xl">
+        <p className="text-base p-10 text-wrap">
+          A group of young people eager to enact growing change in the Devonport
+          Takapuna comunity following in youth voices and youth leadership.
+        </p>
+      </div>
     </div>
   );
 }
--- next/components/projects/YearProjects.tsx
+++ next/components/projects/YearProjects.tsx
@@ -7,9 +7,9 @@ export default function YearProjects({
 }) {
   return (
     <div className="flex flex-col mt-header items-center justify-center text-center">
-      <p className="text-6xl font-bold leading-[0.95]">{year} PROJECTS</p>
+      <p className="text-6xl font-bold leading-[0.95]">CURRENT PROJECTS</p>
       <div className="max-w-4xl">
-        <p className="text-base p-7 text-wrap">
+        <p className="text-base p-10 text-wrap">
           A group of young people eager to enact growing change in the Devonport
           Takapuna comunity following in youth voices and youth leadership.
         </p>
