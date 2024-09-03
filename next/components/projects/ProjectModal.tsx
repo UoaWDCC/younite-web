a0a0e679eda4abf37aa8cdbadc1b4d354ba5e603
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-09-04 03:38:27 +1200

    added transparent  white border

--- next/components/projects/ProjectModal.tsx
+++ next/components/projects/ProjectModal.tsx
@@ -12,10 +12,18 @@ interface ModalProps {
 
 const ProjectModal: React.FC<ModalProps> = ({ title, description, imageUrl, signUpUrl }) => {
   return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 ">
+        <div className="rounded-lg overflow-hidden w-96 relative shadow-lg"
+        style = {{
+          border: '10px solid rgba(255, 255, 255, 0.67)',
+        }}>
+          <button
+            // onClick={onClose}
+            className="absolute top-4 right-4 text-2xl text-white z-30">
+              &#x2715;
+            </button>
 
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-        <div className="bg-white bg-opacity-67 rounded-lg overflow-hidden w-96 relative shadow-lg">
-          <div className="relative w-full h-32 relative z-20">
+          <div className="relative w-full h-32 relative z-20 ">
             <Image
               src={imageUrl}
               alt="Project Image"
@@ -27,7 +35,7 @@ const ProjectModal: React.FC<ModalProps> = ({ title, description, imageUrl, sign
         <div className="p-6"
         style={{
             background: 'linear-gradient(to top, #A2D6E5, #FABD6E)'}}>
-          <h2 className="text-white text-xl font-bold mb-4 text-center relative z-20"> {title} </h2>
+          <h2 className="text-white text-[30px] font-bold mb-4 text-center relative z-20"> {title} </h2>
           <p className="text-white text-center mb-6 relative z-20"> {description}  </p>
 
           <a
@@ -57,13 +65,6 @@ const ProjectModal: React.FC<ModalProps> = ({ title, description, imageUrl, sign
             </div>
 
         </div>
-
-        {/* <button
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
-        >
-          &#x2715;
-        </button> */}
-
       </div>
     </div>
   );
