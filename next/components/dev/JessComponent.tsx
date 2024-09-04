2db01b406b0be11efe835f0e349354c8e4d7dc4a
belleyong <136943913+belleyong@users.noreply.github.com>
2024-09-04 13:45:40 +1200

    fixed the multiple links issue

--- next/components/dev/JessComponent.tsx
+++ next/components/dev/JessComponent.tsx
@@ -1,4 +1,4 @@
-import ProjectModal from '../projects/ProjectModal';
+import ProjectModal from "@/components/projects/ProjectModal";
 
 export default function JessComponent() {
     return (
@@ -9,7 +9,7 @@ export default function JessComponent() {
             Devonport-Takapuna community. Believing in youth voices and youth
             leadership."
         imageUrl="http://127.0.0.1:1337/uploads/priscilla_du_preez_n_F8xh_L_Mmg0c_unsplash_1_7b7bcfcb87.png"
-        signUpUrl="https://www.google.com"/>
+        signUpUrl={["https://www.google.com", "https://www.youtube.com"]}/>
       </div>
     );
   }
