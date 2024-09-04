0cf9a40450fc24419fb9e7dee43450c400e64f67
Oculux <nate.williamson.nz@gmail.com>
2024-09-04 23:13:16 +1200

    :lipstick: add some (very important) fun status messages

--- next/components/feedback/FeedbackForm.tsx
+++ next/components/feedback/FeedbackForm.tsx
@@ -3,31 +3,31 @@ import { useState } from "react";
 import ResponseStatus from "./ResponseStatus";
 
 export default function FeedbackForm() {
-  const [submit, useSubmit] = useState<"true" | "false" | "finished" | "error">(
-    "false",
-  );
+  const [submitState, setSubmitState] = useState<
+    "true" | "false" | "finished" | "error"
+  >("false");
 
   async function handleSubmit(data: FormData) {
-    "use client";
     try {
-      if (submit === "true") {
-        useSubmit("finished");
-      } else if (submit === "false") {
+      if (submitState === "true") {
+        setSubmitState("finished");
+      } else if (submitState === "false") {
         const name = data.get("name") as string;
         const email = data.get("email") as string;
         const message = data.get("message") as string;
         const res = await sendEmail(name, email, message);
 
         if (res === 201) {
-          useSubmit("true");
+          setSubmitState("true");
         } else {
-          useSubmit("error");
+          setSubmitState("error");
         }
       } else {
-        useSubmit("false");
+        setSubmitState("false");
       }
     } catch (err) {
-      useSubmit("error");
+      console.error(err);
+      setSubmitState("error");
     }
   }
 
@@ -58,14 +58,13 @@ export default function FeedbackForm() {
             placeholder="Enter your feedback"
             className="mb-4 bg-white px-4 py-2 rounded-md shadow-md text-b-dark-blue"
           ></textarea>
-          <ResponseStatus submit={submit} />
+          <ResponseStatus submit={submitState} />
         </div>
         <button
-          className="font-bold first:mx-auto px-12 py-3 bg-b-blue text-b-dark-blue rounded-full shadow-md cursor-pointer"
+          className="font-bold first:mx-auto px-12 py-3 bg-b-blue text-b-dark-blue rounded-full shadow-md cursor-pointer uppercase"
           type="submit"
-          value="SUBMIT"
         >
-          SUBMIT
+          {submitState === "false" || submitState === "true" ? "Send!" : "Try again?"}
         </button>
       </form>
     </>
--- next/components/feedback/ResponseStatus.tsx
+++ next/components/feedback/ResponseStatus.tsx
@@ -1,24 +1,32 @@
 import { IoBanSharp, IoCheckmarkCircle } from "react-icons/io5";
+
 export default function ResponseStatus({ submit }: { submit: string }) {
   if (submit == "true") {
     return (
       <div className="mb-4 flex items-center animate-[fadeIn_0.3s_ease-in_forwards]">
         <IoCheckmarkCircle />
-        <p>Your response has been recorded!</p>
+        <p className="whitespace-pre"> Got it! Thanks for getting in touch!</p>
       </div>
     );
   } else if (submit == "finished") {
     return (
       <div className="mb-4 flex items-center animate-[fadeIn_0.3s_ease-in_forwards]">
         <IoBanSharp />
-        <p>You have already submitted this response.</p>
+        <p className="whitespace-pre">
+          {" "}
+          Heads up! You've already sent this response.
+        </p>
       </div>
     );
   } else if (submit == "error") {
     return (
       <div className="mb-4 flex items-center animate-[fadeIn_0.3s_ease-in_forwards]">
         <IoBanSharp />
-        <p>Server error. Please try again later.</p>
+        <p className="whitespace-pre">
+          {" "}
+          Whoops! Something went wrong there. Check you've got wifi and
+          gave us the right email!
+        </p>
       </div>
     );
   } else {
