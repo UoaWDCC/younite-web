a05688e4be314e23c65911db5d6b3272efa1ded7
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-09-11 14:44:58 +1200

    Really bad implem for now, but i got a border and background, some text and thats.

--- next/components/about/ValueModal.tsx
+++ next/components/about/ValueModal.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 interface ModalProps {
@@ -7,11 +8,40 @@ interface ModalProps {
   imageUrl: string;
 }
 
-const ValueModal: React.FC<ModalProps> = ({ title, description, imageUrl = [] }) => {
+const ValueModal: React.FC<ModalProps> = ({ title, description, imageUrl }) => {
   return (
-    <div>
-        <h1>Still in Learning Progress</h1>
-        <p>Remember Practice makes Perfect! 🌟</p>
+    <div className="fixed inset-0 flex items-center justify-center z-50 ">
+      <div className="rounded-lg overflow-hidden w-[44rem] h-[27rem] relative"
+      style = {{
+        border: '12px solid rgba(255, 255, 255)',
+        background: 'linear-gradient(to top, #A2D6E5, #FABD6E)'
+      }}>
+
+        <button
+          // onClick={onClose}
+          className="absolute top-2 right-4 text-2xl font-bold text-white z-30">
+            &#x2715;
+          </button>
+
+        <div className="flex w-1/2 items-center ">
+            <div>
+              <h1 className="font-bold text-[28px] text-white mb-4">{title}</h1>
+              <p className="text-white text-base">{description}</p>
+            </div>
+
+            <div className="relative w1/2">
+              <Image
+                src={imageUrl}
+                alt="Value Image"
+                width={500}
+                height={500}
+              />
+
+              {/* Transparent overlay on image*/}
+              <div className="absolute inset-0 bg-white opacity-30 rounded-xl"></div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
--- next/components/dev/BelleComponent.tsx
+++ next/components/dev/BelleComponent.tsx
@@ -5,7 +5,7 @@ export default function BelleComponent() {
         <div>
         <ValueModal
             title="Youth Voice"
-            description="Sample Description"
+            description="We are a group of young people who are passionate about creating positive change in our communities and beyond."
             imageUrl="http://127.0.0.1:1337/uploads/priscilla_du_preez_n_F8xh_L_Mmg0c_unsplash_1_7b7bcfcb87.png"
         />
       </div>
