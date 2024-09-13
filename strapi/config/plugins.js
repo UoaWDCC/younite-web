22fe18529fc9358a0eda83f717c02619d2622ed6
Oculux <nate.williamson.nz@gmail.com>
2024-09-13 23:38:06 +1200

    :rocket: re-enable minIO bucket

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => {
   const productionConfig =
-    process.env.NODE_ENV === "test" // Should be process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === "production"
       ? {
           upload: {
             config: {
