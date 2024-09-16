847eb326f7d902e0a0f465b28462299da2b1d1ff
h4yleysh4rpe <sharpeh42@gmail.com>
2024-09-16 21:18:41 +1200

    :art: move timeline elements into timeline component

--- next/app/test/projects_timeline/page.tsx
+++ next/app/test/projects_timeline/page.tsx
@@ -1,26 +1,29 @@
 import DateBlob from "@/components/projects/DateBlob";
 import Timeline from "@/components/projects/Timeline";
 
+
+const timelineElements = [
+  { date: new Date("2023-12-25") },
+  { date: new Date("2023-10-27") },
+  { date: new Date("2023-11-30") },
+  { date: new Date("2023-11-30") },
+  { date: new Date("2023-11-30") },
+  { date: new Date("2023-11-30") },
+  { date: new Date("2023-11-30") },
+];
+
 export default async function TestPage() {
-  var timelineElements = [
-    { date: new Date("2023-12-25") },
-    { date: new Date("2023-10-27") },
-    { date: new Date("2023-11-30") },
-    { date: new Date("2023-11-30") },
-    { date: new Date("2023-11-30") },
-    { date: new Date("2023-11-30") },
-    { date: new Date("2023-11-30") },
-  ];
 
   return (
-    <div className="flex flex-col mt-header pl-[10%] pt-[5%] pb-[5%]">
-      <div className="h-4 w-4 bg-white rounded-full"></div>
+    // <div className="flex flex-col mt-header pl-[10%] pt-[5%] pb-[5%]">
+      <Timeline timelineElements={timelineElements} />
+      /* <div className="h-4 w-4 bg-white rounded-full"></div>
       <div>
         {timelineElements.map((timelineElement, index) => (
           <DateBlob timelineElement={timelineElement} isEven={index % 2 == 0} />
         ))}
       </div>
-      <div className="h-4 w-4 bg-white rounded-full"></div>
-    </div>
+      <div className="h-4 w-4 bg-white rounded-full"></div> */
+    // </div>
   );
 }
--- next/components/projects/Timeline.tsx
+++ next/components/projects/Timeline.tsx
@@ -1,10 +1,23 @@
-const Timeline = ({}) => {
+import DateBlob from "@/components/projects/DateBlob";
+
+
+const Timeline = ({timelineElements} : { timelineElements: { date: Date }[] }) => {
+  console.log(timelineElements);
   return (
-    <div className="flex flex-col relative -right-2 mt-header items-center justify-end h-96">
+      <div className="flex flex-col mt-header pl-[10%] pt-[5%] pb-[5%]">
+      <div className="h-4 w-4 bg-white rounded-full"></div>
+      <div>
+        {timelineElements.map((timelineElement, index) => (
+          <DateBlob timelineElement={timelineElement} isEven={index % 2 == 0} />
+        ))}
+      </div>
+      <div className="h-4 w-4 bg-white rounded-full"></div>
+    </div>
+      /* <div className="flex flex-col relative -right-2 mt-header items-center justify-end h-96">
       <div className="h-4 w-4 bg-white rounded-full"></div>
       <div className="w-1 h-full bg-white"></div>
       <div className="h-4 w-4 bg-white rounded-full"></div>
-    </div>
+    </div> */
   );
 };
 
