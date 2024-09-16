3fa462616c71513f17f56feaaa6600b654098837
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-09-16 22:09:29 +1200

    removed the new schema in projects

--- next/schemas/collection/Project.ts
+++ next/schemas/collection/Project.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 
-// What it was before:
 export const projectSchema = z.object({
   slug: z.string(),
   title: z.string(),
@@ -11,17 +10,3 @@ export const projectSchema = z.object({
 });
 
 export type Project = z.infer<typeof projectSchema>;
-
-
-// Changing it for us: removing slug, date, and blocks? Add signup url and link
-// What does blocks do im not sure, ask someone lol
-export const projectModalSchema = z.object({
-  title: z.string(),
-  image: z.any(),
-  description: z.string(),
-  signupurl: z.array(z.string()),
-  signuptext: z.array(z.string())
-});
-
-export type ProjectModalType = z.infer<typeof projectModalSchema>;
-
