e82321a4a15a54021168e3c4be312a30bbbfa60e
belleyong <136943913+belleyong@users.noreply.github.com>
2024-09-18 13:05:08 +1200

    Moved the text to the middle of the value card modal

--- next/components/about/ValueModal.tsx
+++ next/components/about/ValueModal.tsx
@@ -10,8 +10,8 @@ interface ModalProps {
 
 const ValueModal: React.FC<ModalProps> = ({ title, description, imageUrl }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 ">
-      <div className="rounded-lg overflow-hidden w-[44rem] h-[27rem] relative"
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="rounded-xl overflow-hidden w-[44rem] h-[27rem] relative flex"
       style = {{
         border: '12px solid rgba(255, 255, 255)',
         background: 'linear-gradient(to top, #A2D6E5, #FABD6E)'
@@ -23,23 +23,23 @@ const ValueModal: React.FC<ModalProps> = ({ title, description, imageUrl }) => {
             &#x2715;
           </button>
 
-        <div className="flex w-1/2 items-center ">
-            <div>
-              <h1 className="font-bold text-[28px] text-white mb-4">{title}</h1>
-              <p className="text-white text-base">{description}</p>
-            </div>
+        <div className="flex-1 flex flex-col justify-center p-8">
+          <h1 className="font-bold text-[30px] text-white mb-4">{title}</h1>
+          <p className="text-white text-base">{description}</p>
+        </div>
 
-            <div className="relative w1/2">
+            <div className="flex-1 relative">
+              <div className="w-full h-full overflow-hidden rounded-lg" style={{clipPath: 'polygon(30% 0%, 100% 100%, 0%, 100%)'}}>
               <Image
                 src={imageUrl}
                 alt="Value Image"
                 width={500}
                 height={500}
               />
+            </div>
 
               {/* Transparent overlay on image*/}
               <div className="absolute inset-0 bg-white opacity-30 rounded-xl"></div>
-            </div>
           </div>
         </div>
     </div>
