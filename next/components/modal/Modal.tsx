91ec08cfee35ea0a82d4815a0b4d9e8c251cdbb6
Oculux <nate.williamson.nz@gmail.com>
2024-09-20 19:35:14 +1200

    :sparkles: integrate modal context into modal

--- next/components/modal/Modal.tsx
+++ next/components/modal/Modal.tsx
@@ -1,25 +1,24 @@
 import React from "react";
+import { useModal } from "./ModalContextProvider";
 
-export default function Modal({
-  isOpen,
-  onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) {
+export default function Modal() {
+
+  const { isOpen, content, close } = useModal();
+
   if (!isOpen) {
     return null;
   }
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 animate-popUp backdrop-blur-md">
       <div className="bg-gray-600 p-6 rounded-lg shadow-lg relative">
         <button
-          onClick={onClose}
+          onClick={close}
           className="absolute top-2 right-2 text-white hover:text-gray-800"
         >
           &times;
         </button>
-        {/* Pass in contents here! */}
+        {content}
       </div>
     </div>
   );
