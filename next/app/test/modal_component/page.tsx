9e2c420188269adf7eeb8610f99878e4fcd9cff3
Oculux <nate.williamson.nz@gmail.com>
2024-09-20 19:16:16 +1200

    :bricks: move modal into dedicated directory

--- next/app/test/modal_component/page.tsx
+++ next/app/test/modal_component/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Modal from "@/components/blocks/Modal";
+import Modal from "@/components/modal/Modal";
 
 export default function Home() {
   const [isModalOpen, setModalOpen] = useState(false);
