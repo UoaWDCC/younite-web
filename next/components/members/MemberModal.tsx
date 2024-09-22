855915a0775ec9241c3dd1b500697c1b3e4a58d2
h4yleysh4rpe <sharpeh42@gmail.com>
2024-09-22 21:36:18 +1200

    :lipstick: move animations into global modal

--- next/components/members/MemberModal.tsx
+++ next/components/members/MemberModal.tsx
@@ -4,30 +4,14 @@ import { IoClose } from "react-icons/io5";
 import { motion } from "framer-motion";
 
 function MemberModal({
-  activeMember,
+  activeMember
 }: {
   activeMember?: Member;
 }) {
   if (!activeMember) return null;
 
   return (
-    <div className="fixed inset-0 grid place-items-center isolate">
-      <motion.div
-        className="absolute -z-10 w-lvh h-lvh md:w-full md:h-full bg-black backdrop-blur-md bg-opacity-40"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ type: "spring", bounce: 0, duration: 0.3 }}
-      ></motion.div>
-      <motion.div
-        className="md:h-fit md:w-full h-lvh w-lvh md:max-w-6xl grid grid-cols-[2fr_3fr] text-b-dark-blue"
-        style={{
-          background:
-            "radial-gradient(47.21% 33.08% at 23.96% 39.49%, rgba(253, 141, 93, 0.20) 0%, rgba(255, 255, 255, 0.00) 100%), radial-gradient(66.83% 44.73% at 78.61% 81.23%, rgba(250, 171, 54, 0.20) 0%, rgba(255, 255, 255, 0.00) 97.76%), linear-gradient(180deg, #FFF 0%, #F6D09E 100%)",
-        }}
-        initial={{ opacity: 0, y: 16 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ type: "spring", bounce: 0, duration: 0.15 }}
-      >
+    <div>
         <Image
           className="max-h-[80vh] w-full object-cover"
           src="http://127.0.0.1:1337/uploads/priscilla_du_preez_n_F8xh_L_Mmg0c_unsplash_1_7b7bcfcb87.png" //Instead of URL, its supposed to be src={getLargestImageUrl(member.Photo)}, but it does not work
@@ -46,7 +30,6 @@ function MemberModal({
             {activeMember.funFact}
           </p>
         </div>
-      </motion.div>
       <button className="absolute top-4 right-4">
         <IoClose className="w-10 h-10" />
       </button>
--- next/components/modal/Modal.tsx
+++ next/components/modal/Modal.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { motion } from "framer-motion";
 import { useModal } from "./ModalContextProvider";
 
 export default function Modal() {
@@ -10,18 +10,31 @@ export default function Modal() {
   }
 
   return (
-    <div
-      className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 animate-popUp backdrop-blur-md z-50"
-      onClick={close}
+    <motion.div
+      className="absolute w-lvh h-lvh md:w-full md:h-full bg-black backdrop-blur-md bg-opacity-40"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ type: "spring", bounce: 0, duration: 0.3 }}
     >
       <div
-        className="rounded-lg overflow-hidden w-[27rem] relative shadow-lg border-[12px] border-slate-200 hover:border-slate-100"
-        onClick={(e) => e.stopPropagation()}
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-md z-50"
+        onClick={close}
       >
-        <CloseButton onClick={close} />
-        {content}
+        <motion.div
+          initial={{ opacity: 0, y: 16 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ type: "spring", bounce: 0, duration: 0.15 }}
+        >
+          <div
+            className="rounded-lg overflow-hidden w-[27rem] relative shadow-lg border-[12px] border-slate-200 hover:border-slate-100"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <CloseButton onClick={close} />
+            {content}
+          </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
