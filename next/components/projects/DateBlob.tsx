918c2f51002ba6b02a7a9f7e2048f886ff28939a
Oculux <nate.williamson.nz@gmail.com>
2024-09-24 18:20:04 +1200

    :bug: fix date hydration error by enforcing specific date format

--- next/components/projects/DateBlob.tsx
+++ next/components/projects/DateBlob.tsx
@@ -16,13 +16,17 @@ const DateBlob = ({
   isEven: boolean;
 }) => {
   return (
-    // change outer height element to allow for photo height etc
+    // change outer height element to allow for photo height etcs
     <div className="flex flex-row items-center pl-[6px] h-48">
       <div className="bg-white w-1 h-full mt-0"></div>
       <div className="flex flex-row items-center h-8">
         <div className={`bg-white h-1 ${isEven ? "w-32" : "w-20"}`}></div>
         <div className="flex items-center justify-center bg-white w-32 h-full rounded-3xl text-black text-sm font-bold">
-          {timelineElement.Date.toLocaleDateString()}
+          {timelineElement.Date.toLocaleDateString("en-NZ", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
         </div>
       </div>
     </div>
