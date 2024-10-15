2b8a8fe142ae3fac3a9855b56cee90268e0b5aae
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-10-16 03:18:53 +1300

    Styling changes, (also 2023 image not showing up but 2024 is)

--- next/components/members/MemberModal.tsx
+++ next/components/members/MemberModal.tsx
@@ -4,25 +4,27 @@ import Image from "next/image";
 
 function MemberModal({ activeMember }: { activeMember: Member }) {
   return (
-    <div>
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-30">
+      <div className="flex flex-row items-center w-[60rem] h-[32rem] bg-gradient-to-t from-[#F6D09E] to-[#e9e6e1] ">
       <Image
-        className="max-h-[80vh] w-full object-cover"
+        className="max-h-[80vh] w-3/5 object-cover"
         src={getLargestImageUrl(activeMember.Photo)}
-        alt=""
+        alt={activeMember.Name}
         objectFit="cover"
-        width={100}
-        height={100}
+        width={150}
+        height={150}
       />
-      <div className="px-8 py-12 w-[65%]">
+      <div className="text-[#014788] ml-5 px-8 py-12 w-[95%]">
         <h2 className="mb-4 text-3xl font-bold">{activeMember.Name}</h2>
         <h3 className="mb-8 text-lg font-bold max-w-[18ch]">
           {activeMember.Role}
         </h3>
-        <p className="mb-12 max-w-[75ch]">{activeMember.About}</p>
+        <p className="mb-12 max-w-[75ch] text-[15px]"> {activeMember.About}</p>
         <p className="max-w-[75ch] italic text-[14px]">
           {activeMember.funFact}
         </p>
       </div>
+    </div>
     </div>
   );
 }
