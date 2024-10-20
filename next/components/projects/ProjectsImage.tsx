dac71f3cadec8e0072f8d04c1270fa2508cd4da6
unknown <annlowe789@gmail.com>
2024-10-20 22:14:11 +1300

    add current project text styling

--- next/components/projects/ProjectsImage.tsx
+++ next/components/projects/ProjectsImage.tsx
@@ -19,17 +19,23 @@ export default function ImageComponent({
         <Image
           src={src}
           alt={alt || ""}
-          className="absolute inset-0 -z-10 w-full h-full object-cover brightness-50 rounded-[40px]"
-          width={600}
+          className="absolute inset-0 -z-10 w-full h-full object-cover brightness-50 rounded-[50px]"
+          width={300}
           height={height}
         />
       </div>
 
-      <div className="absolute top-[15%] bottom-[15%] right-[15%] w-[35%] bg-black opacity-70 rounded-[20px] z-10">
-        <div className="text-lg pt-2 pb-3 pl-5 pr-5 z-20 opacity-100 text-right">
+      <div className="absolute top-[30%] bottom-[30%] left-[15%] bg-black opacity-70 rounded-[10px] z-10 flex items-center">
+        <div className="text-lg pt-2 pb-3 pl-5 pr-5 z-20 opacity-100 font-bold">
           {title}
         </div>
       </div>
+
+      {/* <div className="absolute top-[15%] bottom-[15%] right-[15%] w-[35%] bg-black opacity-70 rounded-[15px] z-10">
+        <div className="text-lg pt-2 pb-3 pl-5 pr-4 z-20 opacity-100 text-right font-semibold">
+          {title}
+        </div>
+      </div> */}
     </div>
   );
 }
