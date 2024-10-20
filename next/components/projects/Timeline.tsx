375ea98e15b9710f776bee689a36e26450ca07a9
unknown <annlowe789@gmail.com>
2024-10-20 23:02:25 +1300

    :bug: fix project type passing

--- next/components/projects/Timeline.tsx
+++ next/components/projects/Timeline.tsx
@@ -9,23 +9,30 @@ const Timeline = ({
   timelineElements: ProjectType[];
 }) => {
 
+  const firstDay = new Date(new Date().getFullYear(), 0, 1);
+
   return (
     <div className="flex flex-col pl-[10%] pt-[5%] pb-[5%]">
       <div className="h-4 w-4 bg-white rounded-full"></div>
       <div>
-        {timelineElements.map((timelineElement, index) => (
-          <div className="flex items-center gap-20">
-            <DateBlob
-              timelineElement={timelineElement}
-              isEven={index % 2 == 0}
-            />
-            <ImageComponent
-              src={getLargestImageUrl(timelineElement.image)}
-              title={timelineElement.title}
-              type={"current"}
-            />
-          </div>
-        ))}
+        {timelineElements.map((timelineElement, index) => {
+          const date = new Date(timelineElement.Date);
+          const type = date >= firstDay ? "current" : "old";
+
+          return (
+            <div className="flex items-center gap-20">
+              <DateBlob
+                timelineElement={timelineElement}
+                isEven={index % 2 == 0}
+              />
+              <ImageComponent
+                src={getLargestImageUrl(timelineElement.image)}
+                title={timelineElement.title}
+                type={type}
+              />
+            </div>
+          );
+        })}
       </div>
       <div className="h-4 w-4 bg-white rounded-full"></div>
     </div>
