57e4523c81c564ca5c8175c5e3bbe90ce8f93f69
unknown <annlowe789@gmail.com>
2024-10-21 00:42:44 +1300

    :lipstick: fix image styling

--- next/components/projects/ProjectsImage.tsx
+++ next/components/projects/ProjectsImage.tsx
@@ -14,26 +14,29 @@ export default function ImageComponent({
   const height = type === "current" ? 150 : 100;
 
   return (
-    <div className={`w-[600px] h-[${height}px] relative`}>
+    <div
+      className="w-[600px] relative flex justify-center"
+      style={{ height: `${height}px` }}
+    >
       <div>
         <Image
           src={src}
           alt={alt || ""}
-          className="absolute inset-0 -z-10 w-full h-full object-cover brightness-50 rounded-[50px]"
+          className="absolute inset-0 -z-10 w-full h-full object-cover brightness-75 opacity-80 rounded-[50px]"
           width={300}
           height={height}
         />
       </div>
 
       {type === "current" ? (
-        <div className="absolute top-[30%] bottom-[30%] left-[15%] bg-black opacity-70 rounded-[10px] z-10 flex items-center">
-          <div className="text-lg pt-2 pb-3 pl-5 pr-5 z-20 opacity-100 font-semibold">
+        <div className="absolute top-[30%] bottom-[30%] w-[60%] bg-black opacity-70 rounded-[15px] z-10 flex justify-center items-center">
+          <div className="text-2xl pt-3 pb-3 pl-5 pr-5 z-20 opacity-100 text-center font-semibold">
             {title}
           </div>
         </div>
       ) : (
-        <div className="absolute top-[15%] bottom-[15%] right-[15%] w-[35%] bg-black opacity-70 rounded-[15px] z-10">
-          <div className="text-lg pt-2 pb-3 pl-5 pr-4 z-20 opacity-100 text-right font-semibold">
+        <div className="absolute top-[15%] bottom-[15%] right-[18%] w-[25%] bg-black opacity-70 rounded-[15px] z-10">
+          <div className="text-2xl pt-1 pb-1 pl-5 pr-5 z-20 opacity-100 text-right font-semibold">
             {title}
           </div>
         </div>
