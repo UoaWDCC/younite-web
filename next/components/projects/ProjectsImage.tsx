2f2f66d49982176934133af35c749b14de13e3ee
unknown <annlowe789@gmail.com>
2024-10-20 20:12:25 +1300

    :lipstick: add text styling

--- next/components/projects/ProjectsImage.tsx
+++ next/components/projects/ProjectsImage.tsx
@@ -14,21 +14,21 @@ export default function ImageComponent({
   const height = type === "current" ? 150 : 100;
 
   return (
-    <div className={`w-[800px] h-[${height}px] relative`}>
+    <div className={`w-[600px] h-[${height}px] relative`}>
       <div>
         <Image
           src={src}
           alt={alt || ""}
           className="absolute inset-0 -z-10 w-full h-full object-cover brightness-50 rounded-[40px]"
-          width={800}
+          width={600}
           height={height}
         />
       </div>
 
-      <div className="absolute top-[15%] bottom-[15%] right-[15%] w-[35%] bg-black opacity-60 rounded-[20px]"></div>
-
-      <div className="">
-        <div>{title}</div>
+      <div className="absolute top-[15%] bottom-[15%] right-[15%] w-[35%] bg-black opacity-70 rounded-[20px] z-10">
+        <div className="text-lg pt-2 pb-3 pl-5 pr-5 z-20 opacity-100 text-right">
+          {title}
+        </div>
       </div>
     </div>
   );
