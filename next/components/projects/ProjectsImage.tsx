d3acafeda5cacc3c625945571d1a66aee589401b
unknown <annlowe789@gmail.com>
2024-10-21 01:33:59 +1300

    :lipstick: add styling touch ups

--- next/components/projects/ProjectsImage.tsx
+++ next/components/projects/ProjectsImage.tsx
@@ -22,20 +22,20 @@ export default function ImageComponent({
         <Image
           src={src}
           alt={alt || ""}
-          className="absolute inset-0 -z-10 w-full h-full object-cover brightness-75 opacity-80 rounded-[50px]"
+          className="absolute inset-0 -z-10 w-full h-full object-cover brightness-75 opacity-85 rounded-[50px]"
           width={300}
           height={height}
         />
       </div>
 
       {type === "current" ? (
-        <div className="absolute top-[30%] bottom-[30%] w-[60%] bg-black opacity-70 rounded-[15px] z-10 flex justify-center items-center">
+        <div className="absolute top-[30%] bottom-[30%] w-[60%] bg-black bg-opacity-60 rounded-[15px] z-10 flex justify-center items-center">
           <div className="text-2xl pt-3 pb-3 pl-5 pr-5 z-20 opacity-100 text-center font-semibold">
             {title}
           </div>
         </div>
       ) : (
-        <div className="absolute top-[15%] bottom-[15%] right-[18%] w-[25%] bg-black opacity-70 rounded-[15px] z-10">
+        <div className="absolute top-[15%] bottom-[15%] right-[18%] w-[25%] bg-black bg-opacity-50 rounded-[15px] z-10">
           <div className="text-2xl pt-1 pb-1 pl-5 pr-5 z-20 opacity-100 text-right font-semibold">
             {title}
           </div>
