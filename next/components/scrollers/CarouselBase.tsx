9a89fa50f4ddbafdeba63345f65cf776da9bb7ee
daisy-of-the-valley <deizat1111@gmail.com>
2024-10-20 23:34:08 +1300

    :sparkles: add a see more button that leads to projects page

--- next/components/scrollers/CarouselBase.tsx
+++ next/components/scrollers/CarouselBase.tsx
@@ -3,6 +3,7 @@ import useEmblaCarousel from "embla-carousel-react";
 import React, { useCallback } from "react";
 import { BodyScrollLocker } from "./BodyScrollLocker";
 import ScrollButtons from "./CarouselButtons";
+import Link from "next/link";
 
 export default function CarouselBase({
   children,
@@ -36,6 +37,11 @@ export default function CarouselBase({
     <div>
       <div className="text-blue-800 font-semibold text-4xl pr-6 pt-28 flex flex-col sm:flex-row justify-between">
         <p className="text-center text-center md:text-left md:mb-0">Recent Projects</p>
+        <Link
+        href="projects/past"
+        className="text-[14px] px-4 border border-blue-800 rounded-full hover:bg-[#92E0FE] hover:border-[#92E0FE]">
+          See More
+        </Link>
       </div>
       <BodyScrollLocker>
         <div
