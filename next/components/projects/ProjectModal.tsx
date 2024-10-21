87135a144fa8db18535342f81e3220772ca4c13e
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-10-21 15:31:42 +1300

    blob is now clipped

--- next/components/projects/ProjectModal.tsx
+++ next/components/projects/ProjectModal.tsx
@@ -11,6 +11,7 @@ export type ModalProps = {
   }[];
 };
 
+
 const ProjectModal = ({
   title,
   description,
@@ -19,47 +20,46 @@ const ProjectModal = ({
 }: ModalProps) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center">
-      <div className="relative w-[32rem] h-auto z-10 p-6 bg-white rounded-xl shadow-lg  overflow-hidden">
-      <div className="relative w-full h-[9rem] z-10">
-        <Image
-          src={imageUrl}
-          alt="Project Image"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
+      <div className="relative w-[32rem] h-auto z-10 p-6 bg-white border border-gray-200 rounded-xl shadow-lg overflow-hidden">
+        <div className="relative w-full h-[9rem] z-10">
+          <Image
+            src={imageUrl}
+            alt="Project Image"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
 
-      <div className="p-6 -z-50 bg-[linear-gradient(to_top,#A2D6E5,#FABD6E)]">
-        <h1 className="text-white text-[27px] text-xl font-black mb-5 mt-2 text-center relative z-10">
-          {title}
-        </h1>
-        <p className="text-white text-[16px] text-center mb-6 relative z-10">
-          {description}
-        </p>
+        <div className="relative p-6 bg-[linear-gradient(to_top,#A2D6E5,#FABD6E)] overflow-hidden">
+          <h1 className="text-white text-[27px] text-xl font-black mb-5 mt-2 text-center relative z-10">
+            {title}
+          </h1>
+          <p className="text-white text-[16px] text-center mb-6 relative z-10">
+            {description}
+          </p>
 
-        {/* sign up buttons links */}
-        {signupobjects.map((signupobject, index) => (
-          <div key={index} className="text-center">
-            <a
-              href={signupobject.signUpUrl}
-              className="mx-auto block w-48 text-[12.5px] mt-2 font-bold bg-white text-black text-center py-2 rounded-3xl relative -10"
-              target="sign up button"
-            >
-              {signupobject.signUpText}
-            </a>
+          {/* sign up buttons links */}
+          {signupobjects.map((signupobject, index) => (
+            <div key={index} className="text-center">
+              <a
+                href={signupobject.signUpUrl}
+                className="mx-auto block w-48 text-[12.5px] mt-2 font-bold bg-white text-black text-center py-2 rounded-3xl relative"
+                target="sign up button"
+              >
+                {signupobject.signUpText}
+              </a>
+            </div>
+          ))}
+
+          {/* Add the blobs */}
+          <div className="absolute -bottom-8 -left-8 z-0">
+            <Image src={blobleft} alt="BlobLeft" width={140} height={140} />
           </div>
-        ))}
 
-        {/* Add the blobs */}
-
-        <div className="absolute bottom-10 top-43 -left-9 z-0">
-          <Image src={blobleft} alt="BlobLeft" width={140} height={140} />
+          <div className="absolute -top-8 -right-8 z-0">
+            <Image src={blobright} alt="BlobRight" width={140} height={140} />
+          </div>
         </div>
-
-        <div className="absolute -bottom-20 top-20 -right-5 z-0">
-          <Image src={blobright} alt="BlobRight" width={140} height={140} />
-        </div>
-      </div>
       </div>
     </div>
   );
