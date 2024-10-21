8db692d813b7a95f50499d6753895dd65a0f065d
daisy-of-the-valley <deizat1111@gmail.com>
2024-10-21 16:27:56 +1300

    :lipstick: changed project card styling (text)

--- next/components/home/carousel/ProjectCard.tsx
+++ next/components/home/carousel/ProjectCard.tsx
@@ -22,8 +22,8 @@ export default function ProjectCard({ name, date, img }: ProjectCardProps) {
       </div>
 
       <div className="h-20 rounded-5x1 bg-white">
-        <div className="pl-3 pt-0.5 font-semibold text-lg">{name}</div>
-        <div className="pl-3 text-base">{date.toDateString().substring(3)}</div>
+        <div className="pl-3 pt-3 pt-0.5 font-semibold text-lg leading-5 text-ellipsis line-clamp-2">{name}</div>
+        <div className="pl-3 pt-1 text-base">{date.toDateString().substring(3)}</div>
       </div>
     </div>
 
