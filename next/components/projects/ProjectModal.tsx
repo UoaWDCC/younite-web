ae0959492c276b70284834ac967d96a69ec1bfa2
Jessica Zeng <szen566@aucklanduni.ac.nz>
2024-10-21 15:47:25 +1300

    turns out i was doing it wrong, need to test on the project page

--- next/components/projects/ProjectModal.tsx
+++ next/components/projects/ProjectModal.tsx
@@ -11,7 +11,6 @@ export type ModalProps = {
   }[];
 };
 
-
 const ProjectModal = ({
   title,
   description,
@@ -19,51 +18,48 @@ const ProjectModal = ({
   signupobjects = [],
 }: ModalProps) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-       <div
-        className="bg-white bg-opacity-67 rounded-lg overflow-hidden w-[32rem] relative shadow-lg"
-        style={{
-          border: '12px solid rgba(255, 255, 255, 0.6)', // 6px thick border with 30% opacity
-        }}>
-        <div className="relative w-full h-[9rem] z-10">
-          <Image
-            src={imageUrl}
-            alt="Project Image"
-            layout="fill"
-            objectFit="cover"
-          />
+    <div className="inset-0 flex items-center justify-center">
+      <div className="relative w-[32rem] h-auto z-10 p-6ounded-xl overflow-hidden">
+      <div className="relative w-full h-[9rem] z-10">
+        <Image
+          src={imageUrl}
+          alt="Project Image"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+
+      <div className="p-6 relative bg-[linear-gradient(to_top,#A2D6E5,#FABD6E)]">
+        <h1 className="text-white text-[27px] text-xl font-black mb-5 mt-2 text-center relative z-10">
+          {title}
+        </h1>
+        <p className="text-white text-[16px] text-center mb-6 relative z-10">
+          {description}
+        </p>
+
+        {/* sign up buttons links */}
+        {signupobjects.map((signupobject, index) => (
+          <div key={index} className="text-center">
+            <a
+              href={signupobject.signUpUrl}
+              className="mx-auto block w-48 text-[12.5px] mt-2 font-bold bg-white text-black text-center py-2 rounded-3xl relative -10"
+              target="sign up button"
+            >
+              {signupobject.signUpText}
+            </a>
+          </div>
+        ))}
+
+        {/* Add the blobs */}
+
+        <div className="absolute -bottom-8 -left-8 z-0">
+          <Image src={blobleft} alt="BlobLeft" width={140} height={140} />
         </div>
 
-        <div className="relative p-6 bg-[linear-gradient(to_top,#A2D6E5,#FABD6E)] overflow-hidden">
-          <h1 className="text-white text-[27px] text-xl font-black mb-5 mt-2 text-center relative z-10">
-            {title}
-          </h1>
-          <p className="text-white text-[16px] text-center mb-6 relative z-10">
-            {description}
-          </p>
-
-          {/* sign up buttons links */}
-          {signupobjects.map((signupobject, index) => (
-            <div key={index} className="text-center">
-              <a
-                href={signupobject.signUpUrl}
-                className="mx-auto block w-48 text-[12.5px] mt-2 font-bold bg-white text-black text-center py-2 rounded-3xl relative"
-                target="sign up button"
-              >
-                {signupobject.signUpText}
-              </a>
-            </div>
-          ))}
-
-          {/* Add the blobs */}
-          <div className="absolute -bottom-8 -left-8 z-0">
-            <Image src={blobleft} alt="BlobLeft" width={140} height={140} />
-          </div>
-
-          <div className="absolute -top-8 -right-8 z-0">
-            <Image src={blobright} alt="BlobRight" width={140} height={140} />
-          </div>
+        <div className="absolute -top-8 -right-8 z-0">
+          <Image src={blobright} alt="BlobRight" width={140} height={140} />
         </div>
+      </div>
       </div>
     </div>
   );
